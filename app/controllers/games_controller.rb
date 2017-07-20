d8f2674e62fd3e97b58985d5959e1a5404dce367
MelsenC <melsen.carlsburg@gmail.com>
2017-07-20 02:33:02 +0000

    ran rubocop

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,9 @@
 class GamesController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
-  def new
-  end
+  def new; end
 
-  def create
-  end
+  def create; end
 end
