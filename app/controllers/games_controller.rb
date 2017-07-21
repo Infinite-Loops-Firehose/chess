3a407b652cdf1bd223b85601bd580457d2115deb
Robert Polacheck <rpolacheck@gmail.com>
2017-07-21 05:43:36 +0000

    Changed ID's and centered the board

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,15 @@
 class GamesController < ApplicationController
-  def index; end
 
-  def show; end
+  def index
+  end
+  
+  def show 
+  end
 
-  def new; end
+  def new 
+  end
 
-  def create; end
+  def create 
+  end
+
 end
