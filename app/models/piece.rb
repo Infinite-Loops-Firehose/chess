b576fc43fcc28f7c965f92d2a15583ffefc3c5da
Robert Polacheck <rpolacheck@gmail.com>
2017-07-24 22:49:32 +0000

    Added classes for each of the chess pieces that inherit from the piece class

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,3 +1,10 @@
 class Piece < ApplicationRecord
   belongs_to :game
 end
+
+class Pawn < Piece; end
+class Knight < Piece; end
+class Bishop < Piece; end
+class Rook < Piece; end
+class Queen < Piece; end
+class King < Piece; end
