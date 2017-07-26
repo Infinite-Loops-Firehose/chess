440a5b618a7bab6e556e8a1a8dc38c8c378df098
Dennis Ouyang <den2net@yahoo.com>
2017-07-26 02:37:54 +0000

    tests passing for new game creation

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,41 +4,41 @@ class Game < ApplicationRecord
   has_many :pieces
 
   def populate_board!
-    # this should create all 32 pieces with their initial X/Y coordinates.
+    # this should create all 32 Pieces with their initial X/Y coordinates.
 
-    # black pieces
+    # black Pieces
     (1..8).each do |i|
-      Pawn.create(x_position: i, y_position: 2, game_id: id, is_white: false)
+      Piece.create(game_id: id, is_white: false, type: PAWN, x_position: i, y_position: 2)
     end
 
-    Rook.create(x_position: 1, y_position: 1, game_id: id, is_white: false)
-    Rook.create(x_position: 8, y_position: 1, game_id: id, is_white: false)
+    Piece.create(game_id: id, is_white: false, type: ROOK, x_position: 1, y_position: 1)
+    Piece.create(game_id: id, is_white: false, type: ROOK, x_position: 8, y_position: 1)
 
-    Knight.create(x_position: 2, y_position: 1, game_id: id, is_white: false)
-    Knight.create(x_position: 7, y_position: 1, game_id: id, is_white: false)
+    Piece.create(game_id: id, is_white: false, type: KNIGHT, x_position: 2, y_position: 1)
+    Piece.create(game_id: id, is_white: false, type: KNIGHT, x_position: 7, y_position: 1)
 
-    Bishop.create(x_position: 3, y_position: 1, game_id: id, is_white: false)
-    Bishop.create(x_position: 6, y_position: 1, game_id: id, is_white: false)
+    Piece.create(game_id: id, is_white: false, type: BISHOP, x_position: 3, y_position: 1)
+    Piece.create(game_id: id, is_white: false, type: BISHOP, x_position: 6, y_position: 1)
 
-    King.create(x_position: 4, y_position: 1, game_id: id, is_white: false)
-    Queen.create(x_position: 5, y_position: 1, game_id: id, is_white: false)
+    Piece.create(game_id: id, is_white: false, type: KING, x_position: 4, y_position: 1)
+    Piece.create(game_id: id, is_white: false, type: QUEEN, x_position: 5, y_position: 1)
 
-    # white pieces
+    # white Pieces
     (1..8).each do |i|
-      Pawn.create(x_position: i, y_position: 7, game_id: id, is_white: true)
+      Piece.create(game_id: id, is_white: true, type: PAWN, x_position: i, y_position: 7)
     end
 
-    Rook.create(x_position: 1, y_position: 8, game_id: id, is_white: true)
-    Rook.create(x_position: 8, y_position: 8, game_id: id, is_white: true)
+    Piece.create(game_id: id, is_white: true, type: ROOK, x_position: 1, y_position: 8)
+    Piece.create(game_id: id, is_white: true, type: ROOK, x_position: 8, y_position: 8)
 
-    Knight.create(x_position: 2, y_position: 8, game_id: id, is_white: true)
-    Knight.create(x_position: 7, y_position: 8, game_id: id, is_white: true)
+    Piece.create(game_id: id, is_white: true, type: KNIGHT, x_position: 2, y_position: 8)
+    Piece.create(game_id: id, is_white: true, type: KNIGHT, x_position: 7, y_position: 8)
 
-    Bishop.create(x_position: 3, y_position: 8, game_id: id, is_white: true)
-    Bishop.create(x_position: 6, y_position: 8, game_id: id, is_white: true)
+    Piece.create(game_id: id, is_white: true, type: BISHOP, x_position: 3, y_position: 8)
+    Piece.create(game_id: id, is_white: true, type: BISHOP, x_position: 6, y_position: 8)
 
-    King.create(x_position: 4, y_position: 8, game_id: id, is_white: true)
-    Queen.create(x_position: 5, y_position: 8, game_id: id, is_white: true)
+    Piece.create(game_id: id, is_white: true, type: KING, x_position: 5, y_position: 8)
+    Piece.create(game_id: id, is_white: true, type: QUEEN, x_position: 4, y_position: 8)
   end
 end
 
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,3 +1,10 @@
 class Piece < ApplicationRecord
   belongs_to :game
 end
+
+PAWN='Pawn'
+ROOK='Rook'
+KNIGHT='Knight'
+BISHOP='Bishop'
+QUEEN='Queen'
+KING='King'
