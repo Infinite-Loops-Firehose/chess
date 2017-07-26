d9d682d50d0a0603233a5b552eaf44de1bf74a5a
Dennis Ouyang <den2net@yahoo.com>
2017-07-26 04:59:44 +0000

    Fixed broken i% syntax in games_contoller.rb and routes.rb

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,10 @@
 class GamesController < ApplicationController
-  before_action :authenticate_user!, only: %i[:new, :create, :show]
+  before_action :authenticate_user!, only: %i[new create show]
 
   def new; end
 
   def create
     @game = Game.new(game_params)
-    @game.user_white = current_user
     @game.save
     redirect_to game_path(@game)
   end
@@ -13,7 +12,7 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
   end
-  
+
   def game_params
     params.require(:game).permit(:user_white_id, :user_black_id)
   end
