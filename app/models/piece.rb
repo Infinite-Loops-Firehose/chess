2eb5213d67d450c567408beb1a44882d370def16
MelsenC <melsen.carlsburg@gmail.com>
2017-07-28 20:26:59 +0000

    added conditional statements to check if horizontal, vertical, or diagonal before checking for obstructions. finished a first draft of the diagonal obstruction method - still need to test it

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,7 +1,7 @@
 class Piece < ApplicationRecord
   belongs_to :game
 
-# customize this method for each subclass, using only what is needed for that piece's movement
+# customize this method for each subclass (bishop, pawn, etc), using only what is needed for that piece's movement
   # def obstructed?(destination_x, destination_y)
   #  starting_point = [self.x_position, self.y_position]
   #  ending_point = [destination_x, destination_y]
@@ -13,66 +13,41 @@ class Piece < ApplicationRecord
   # end
 
   def horizontal_or_vertical_obstruction?(destination_x, destination_y)
-    range_x = [destination_x, self.x_position].sort
-    range_y = [destination_y, self.y_position].sort
-    obstruction = Piece.where(y_position: ((range_y.first + 1)..(range_y.last - 1)), x_position: ((range_x.first + 1)..(range_x.last - 1))) #will always return something, even if it's an empty query
-    obstruction.present? #should return false if the query is empty    
+    if destination_x == self.x_position || destination_y == self.y_position
+      range_x = [destination_x, self.x_position].sort
+      range_y = [destination_y, self.y_position].sort
+      obstruction = Piece.where(y_position: ((range_y.first + 1)..(range_y.last - 1)), x_position: ((range_x.first + 1)..(range_x.last - 1))) #will always return something, even if it's an empty query
+      obstruction.present? #should return false if the query is empty    
+    else
+      return false
+    end
   end
 
-  # def vertical_obstruction?(destination_x, destination_y)
-  #   obstruction = nil
-  #   if destination_x - self.x_position == 0
-  #     if destination_y > self.y_position
-  #       obstruction = Piece.where('x_position = ? AND y_position < ? AND y_position > ?', self.x_position, destination_y, self.y_position)
-  #     else
-  #       obstruction = Piece.where('x_position = ? AND y_position > ? AND y_position < ?', self.x_position, destination_y, self.y_position)
-  #     end
-  #   end
-  #   if obstruction.present?
-  #     return true
-  #   else
-  #     return false
-  #   end
-  # end
-
   def diagonal_obstruction?(destination_x, destination_y)
-    # obstruction = nil
-    x_vector = destination_x - self.x_position #gets the distance and the direction of the horizontal move.
+    x_vector = destination_x - self.x_position # gets the distance and the direction of the horizontal move.
     # positive is to the right, negative is to the left.
     y_vector = destination_y - self.y_position #gets the distance and the direction of the vertical move.
     # positive is up, negative is down.
-    # if x_vector.abs == y_vector.abs #in order for the move to be diagonal, 
-    # the piece must be moving by the same distance both horizontally and vertically.
+    if x_vector.abs == y_vector.abs #in order for the move to be diagonal, the piece must be moving by the same distance both horizontally and vertically.
+      x_values = (self.x_position..destination_x).to_a # array of x values, including the starting and ending squares
+      y_values = (self.y_position..destination_y).to_a # array of y values, including the starting and ending squares
+      x_y_values = [] # is it necessary to declare this array first before pushing to it?
 
-    # you could use arrays to list all the in-between squares.
-    # then you can get all the x coordinates and x coordinates. Rails can query the x position and y position arrays
-      
-    
+      x_values.zip(y_values) do |x_value, y_value| # combines x_value and y_value pairs into a multidimensional array
+        x_y_values << [x_value, y_value]
+      end
 
-    # the range can be determined by the equation of your line (y = mx + b), and you can iterate over your x values
-    # m is the slope (formula: y2 - y1 / x2 - x1)
-    # start with the x values, then calculate the y values based on the line function
-    
-    x_values = (self.x_position..destination_x).to_a #array of x values, including the starting and ending squares
-    y_values = (self.y_position..destination_y).to_a #array of y values, including the starting and ending squares
+      coordinates = x_y_values.shift.pop # this is an array of only the in-between squares - not including the start or end squares
 
-    x_values[1]..x_values[x_vector.abs - 1] do |x_value|
+      sql = coordinates.map do |coordinate|
+        "x_position = #{coordinate.first} and y_position = #{coordinate.last}"
+      end.join(" or ")
 
+      obstruction = Piece.where(sql)
+      obstruction.present?
+    else
+      return false
     end
-
-    coordinates = [[2, 2], [3, 3], [4, 4]] #just an example, if our starting point is [1, 1] and our ending point is [5, 5]
-    sql = coordinates.map do |coordinate|
-      "x_position = #{coordinate.first} and y_position = #{coordinate.last}"
-    end.join(" or ")
-
-    obstruction = Piece.where(sql)
-        
-
-        # Piece.where('x_position > ? AND y_position < ?', x_destination, y_destination) #left up
-        # Piece.where('x_position < ? AND y_position > ?', x_destination, y_destination) #right down
-        # Piece.where('x_position > ? AND y_position > ?', x_destination, y_destination) #left down
-
-    obstruction.present?
   end
 
   def invalid?(destination_x, destination_y)
@@ -82,27 +57,3 @@ class Piece < ApplicationRecord
     
   end
 end
-
-# chessboard = [ 
-#   [1, 1, 1, 1, 1, 1, 1, 1],
-#   [1, 1, 1, 1, 1, 1, 1, 1],
-#   [0, 0, 0, 0, 0, 0, 0, 0],
-#   [0, 0, 0, 0, 0, 0, 0, 0],
-#   [0, 0, 0, 0, 0, 0, 0, 0],
-#   [0, 0, 0, 0, 0, 0, 0, 0],
-#   [1, 1, 1, 1, 1, 1, 1, 1],
-#   [1, 1, 1, 1, 1, 1, 1, 1]
-# ]
-
-#where 1 = occupied, 0 = unoccupied
-
-# create an initial blank chessboard - all 0's
-# loop through all the pieces
-# for all pieces, take their location and change the location in that empty chessboard to 1 (occupied)
-
-# look at each square in between the starting position and the destination,
-# check if any of those square are occupied
-
-# horizontal => look at the x direction only
-
-# vertical => look at the y direction only
