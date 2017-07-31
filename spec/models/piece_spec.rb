f6d59184fa57cb89383af027dee1c1c574abb4c4
Robert Polacheck <rpolacheck@gmail.com>
2017-07-31 14:08:57 +0000

    Started TDD on obstruction method

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
+  describe '#horizonal_or_vertical_obstruction?' do
+    it 'tests for unimpeded vertical or horizontal movement' do
+      piece_01 = FactoryGirl.build(:piece)
+      piece_02 = FactoryGirl.build(:piece, :x_position => 3, :y_position => 1)
+      expect.horizonal_or_vertical_obstruction?.to be_false
+    end
+  end
+
+  describe '#diagonal_obstruction?' do
+    it 'tests unimpeded diagonal movement' do
+      piece_01 = FactoryGirl.build(:piece)
+      piece_02 = FactoryGirl.build(:piece, :x_position => 3, :y_position => 3)
+      expect.diagonal_obstruction?.to be_false
+    end
+  end
 end
