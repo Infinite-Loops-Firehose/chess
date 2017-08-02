bddcd1b77946624b16439b67e7e2d49003e9a64f
michellejanosi <michelle.janosi@gmail.com>
2017-08-02 09:52:28 -0400

    Changed tests to allow for whatever game id instead of game id as 1

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -3,26 +3,27 @@ require 'rails_helper'
 RSpec.describe Piece, type: :model do
   describe '#obstructed?' do
     let(:game) { create :obstructed_game }
-    
+
     context 'when checking diagonal obstruction' do
       it 'between A6 and C4' do
         game = obstructed_game
-        piece = FactoryGirl.create(:piece, game_id: 1, x_position: 1, y_position: 6)
+        piece = FactoryGirl.create(:piece, game_id: game.id, x_position: 1, y_position: 6)
         expect(piece.obstructed?(3, 4)).to eq(false)
       end
 
       it 'between F1 and D3' do
         game = obstructed_game
-        piece = FactoryGirl.create(:piece, game_id: 1, :x_position => 4, :y_position => 1)
+        piece = FactoryGirl.create(:piece, game_id: game.id, :x_position => 6, :y_position => 1)
         expect(piece.obstructed?(4, 3)).to eq(true)
       end
     end
 
-    # it 'checks for vertical obstruction between A1 and A4' do
-    #   game = FactoryGirl.create_obstructed_game
-    #   piece = FactoryGirl.create(:piece, game_id: game.id, :x_position => 1, :y_position => 1)
-    #   expect(piece.obstructed?(1, 4)).to be_true
-    # end
+    it 'checks for vertical obstruction between A1 and A4' do
+      game = obstructed_game
+      piece = FactoryGirl.create(:piece, game_id: game.id, :x_position => 1, :y_position => 1)
+      piece2 = FactoryGirl.create(:piece, game_id: game.id, :x_position => 1, :y_position => 2)
+      expect(piece.obstructed?(1, 4)).to eq(true)
+    end
 
     # it 'checks for invalid move between D4 and B5' do
     #   game = FactoryGirl.create_obstructed_game
