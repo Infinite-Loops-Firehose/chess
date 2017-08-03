075f7ee58bf13ae243abb6223b795235b1ae6790
michellejanosi <michelle.janosi@gmail.com>
2017-08-03 18:37:07 -0400

    Made syntax corrections

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -3,33 +3,33 @@ require 'rails_helper'
 RSpec.describe King, type: :model do
   describe 'valid_move?' do
     it 'should return true if moving one square up' do
-      game = FactoryGirl.create(:game)
+      FactoryGirl.create(:game)
       king = King.create(x_position: 6, y_position: 3)
-      expect(king.valid_move?(6,4)).to eq true
+      expect(king.valid_move?(6, 4)).to eq true
     end
 
     it 'should return true if moving one square left' do
-      game = FactoryGirl.create(:game)
+      FactoryGirl.create(:game)
       king = King.create(x_position: 6, y_position: 3)
-      expect(king.valid_move?(5,3)).to eq true
+      expect(king.valid_move?(5, 3)).to eq true
     end
 
     it 'should return true if moving one square diagonal' do
-      game = FactoryGirl.create(:game)
+      FactoryGirl.create(:game)
       king = King.create(x_position: 6, y_position: 3)
-      expect(king.valid_move?(7,2)).to eq true
+      expect(king.valid_move?(7, 2)).to eq true
     end
 
     it 'should return false if moving more than one square up' do
-      game = FactoryGirl.create(:game)
+      FactoryGirl.create(:game)
       king = King.create(x_position: 6, y_position: 3)
-      expect(king.valid_move?(6,5)).to eq false
+      expect(king.valid_move?(6, 5)).to eq false
     end
 
     it 'should return false if moving to a random square' do
-      game = FactoryGirl.create(:game)
+      FactoryGirl.create(:game)
       king = King.create(x_position: 6, y_position: 3)
-      expect(king.valid_move?(3,7)).to eq false
+      expect(king.valid_move?(3, 7)).to eq false
     end
   end
 end
