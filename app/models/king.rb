eea0ec17b844b8ecc0e031c5279c2308f18b6f79
michellejanosi <michelle.janosi@gmail.com>
2017-08-03 17:15:15 -0400

    Added starter code

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,1 +1,6 @@
-class King < Piece; end
+class King < Piece
+  def valid_move?(destination_x, destination_y)
+  # return true if the move is valid
+  # return false if the move is invalid
+  end
+end
---
+++ spec/models/king_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe King, type: :model do
+  describe 'valid_move?' do
+    it 'should return true if moving one square' do
+
+    end
+  end
+end
