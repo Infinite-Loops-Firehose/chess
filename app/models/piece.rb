48f1b602a8af9d9380f26bb7f56f678a4a4c56ec
MelsenC <melsen.carlsburg@gmail.com>
2017-08-04 00:30:48 +0000

    updated game_spec.rb to match file that works in the obstructed branch. Also uncommented the .freeze assignments in piece.rb

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,17 +1,6 @@
 class Piece < ApplicationRecord
   belongs_to :game
 
-  # attr_reader :game_id, :is_white, :type
-  # attr_accessor :x_position, :y_position
-
-  # def initialize(game_id, is_white, type, x_position, y_position)
-  #   @game_id = game_id
-  #   @is_white = is_white
-  #   @type = type
-  #   @x_position = x_position
-  #   @y_position = y_position
-  # end
-
   def color
     # needs to be fixed so is_white acts like the attribute it is, and not a method.
     return 'White' if is_white == true
@@ -23,9 +12,9 @@ class Piece < ApplicationRecord
   end
 end
 
-# PAWN = 'Pawn'.freeze
-# ROOK = 'Rook'.freeze
-# KNIGHT = 'Knight'.freeze
-# BISHOP = 'Bishop'.freeze
-# QUEEN = 'Queen'.freeze
-# KING = 'King'.freeze
+PAWN = 'Pawn'.freeze
+ROOK = 'Rook'.freeze
+KNIGHT = 'Knight'.freeze
+BISHOP = 'Bishop'.freeze
+QUEEN = 'Queen'.freeze
+KING = 'King'.freeze
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -11,54 +11,54 @@ RSpec.describe Game, type: :model do
     it 'creates correct number of pawns' do
       game = FactoryGirl.create(:game)
       game.populate_board!
-      n_white_pawns = game.pieces.where(type: PAWN, is_white: true).count
+      n_white_pawns = game.pieces.where(type: Piece::PAWN, is_white: true).count
       expect(n_white_pawns).to eq(8)
-      n_black_pawns = game.pieces.where(type: PAWN, is_white: false).count
+      n_black_pawns = game.pieces.where(type: Piece::PAWN, is_white: false).count
       expect(n_black_pawns).to eq(8)
     end
 
     it 'creates correct number of rooks' do
       game = FactoryGirl.create(:game)
       game.populate_board!
-      n_white_rooks = game.pieces.where(type: ROOK, is_white: true).count
+      n_white_rooks = game.pieces.where(type: Piece::ROOK, is_white: true).count
       expect(n_white_rooks).to eq(2)
-      n_black_pawns = game.pieces.where(type: ROOK, is_white: false).count
-      expect(n_black_pawns).to eq(2)
+      n_black_rooks = game.pieces.where(type: Piece::ROOK, is_white: false).count
+      expect(n_black_rooks).to eq(2)
     end
 
     it 'creates correct number of knights' do
       game = FactoryGirl.create(:game)
       game.populate_board!
-      n_white_knights = game.pieces.where(type: KNIGHT, is_white: true).count
+      n_white_knights = game.pieces.where(type: Piece::KNIGHT, is_white: true).count
       expect(n_white_knights).to eq(2)
-      n_black_knights = game.pieces.where(type: KNIGHT, is_white: false).count
+      n_black_knights = game.pieces.where(type: Piece::KNIGHT, is_white: false).count
       expect(n_black_knights).to eq(2)
     end
 
     it 'creates the correct number of bishops' do
       game = FactoryGirl.create(:game)
       game.populate_board!
-      n_white_bishops = game.pieces.where(type: BISHOP, is_white: true).count
+      n_white_bishops = game.pieces.where(type: Piece::BISHOP, is_white: true).count
       expect(n_white_bishops).to eq(2)
-      n_black_bishops = game.pieces.where(type: BISHOP, is_white: false).count
+      n_black_bishops = game.pieces.where(type: Piece::BISHOP, is_white: false).count
       expect(n_black_bishops).to eq(2)
     end
 
     it 'creates one queen for each side' do
       game = FactoryGirl.create(:game)
       game.populate_board!
-      n_white_queens = game.pieces.where(type: QUEEN, is_white: true).count
+      n_white_queens = game.pieces.where(type: Piece::QUEEN, is_white: true).count
       expect(n_white_queens).to eq(1)
-      n_black_queens = game.pieces.where(type: QUEEN, is_white: false).count
+      n_black_queens = game.pieces.where(type: Piece::QUEEN, is_white: false).count
       expect(n_black_queens).to eq(1)
     end
 
     it 'creates one king for each side' do
       game = FactoryGirl.create(:game)
       game.populate_board!
-      n_white_kings = game.pieces.where(type: KING, is_white: true).count
+      n_white_kings = game.pieces.where(type: Piece::KING, is_white: true).count
       expect(n_white_kings).to eq(1)
-      n_black_kings = game.pieces.where(type: KING, is_white: false).count
+      n_black_kings = game.pieces.where(type: Piece::KING, is_white: false).count
       expect(n_black_kings).to eq(1)
     end
   end
