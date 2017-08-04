723aa48dc8cba18af26f745acd72adddac695f70
MelsenC <melsen.carlsburg@gmail.com>
2017-08-04 00:36:21 +0000

    adjusted game.rb and piece.rb to put the piece type defintions within the Piece class

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,6 +1,13 @@
 class Piece < ApplicationRecord
   belongs_to :game
 
+  PAWN = 'Pawn'.freeze
+  ROOK = 'Rook'.freeze
+  KNIGHT = 'Knight'.freeze
+  BISHOP = 'Bishop'.freeze
+  QUEEN = 'Queen'.freeze
+  KING = 'King'.freeze  
+
   def color
     # needs to be fixed so is_white acts like the attribute it is, and not a method.
     return 'White' if is_white == true
@@ -12,9 +19,3 @@ class Piece < ApplicationRecord
   end
 end
 
-PAWN = 'Pawn'.freeze
-ROOK = 'Rook'.freeze
-KNIGHT = 'Knight'.freeze
-BISHOP = 'Bishop'.freeze
-QUEEN = 'Queen'.freeze
-KING = 'King'.freeze
