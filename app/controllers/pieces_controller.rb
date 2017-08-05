c80fd1b5b44a534cb0d4da8bfece8bfbdec126d7
Robert Polacheck <rpolacheck@gmail.com>
2017-08-05 04:04:50 +0000

    Finished Games.show and working to finish Pieces.show page

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -6,6 +6,6 @@ class PiecesController < ApplicationController
   def update
     piece_to_move = Piece.find(params[:id])
     piece_to_move.move_to!(params[:x_position], params[:y_position])
-    redirect_to game_path(piece_to_move.game.id)
+    redirect_to game_path(piece_to_move.game)
   end
 end
