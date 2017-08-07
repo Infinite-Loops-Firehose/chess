628f7f2995e57252a3d6e00ca1be4586ad1134fc
Dennis Ouyang <den2net@yahoo.com>
2017-08-07 21:17:44 +0000

    first attempt at writing bishop move validation

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,1 +1,8 @@
-class Bishop < Piece; end
+class Bishop < Piece
+  def valid_move?(new_x, new_y)
+    if !(1..8).include?(new_x) || !(1..8).include?(new_y) # || obstructed?(new_x, new_y)
+      return true if (new_x - x_position).abs == (new_y - y_position).abs
+    end
+    false
+  end
+end
---
+++ spec/models/bishop_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'Bishop', type: :model do
+  context '#valid_move for Bishop' do
+
+  end
+end
