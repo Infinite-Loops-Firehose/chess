c5f405ab028a1b19ea60ae515cb5e5b4e424cfbc
Dennis Ouyang <den2net@yahoo.com>
2017-08-08 04:22:34 +0000

    fixed #obstructed? and #diagonal_obstruction?, tested bishop movement

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,7 +1,7 @@
 class Bishop < Piece
   def valid_move?(new_x, new_y)
-    if !(1..8).include?(new_x) || !(1..8).include?(new_y) # || obstructed?(new_x, new_y)
-      return true if (new_x - x_position).abs == (new_y - y_position).abs
+    unless obstructed?(new_x, new_y)
+      return true if (new_x.to_i - x_position).abs == (new_y.to_i - y_position).abs
     end
     false
   end
