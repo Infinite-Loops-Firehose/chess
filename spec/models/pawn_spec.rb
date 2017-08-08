77eeadfd6f26f901bd0c880c7fa050f9c0855f16
michellejanosi <michelle.janosi@gmail.com>
2017-08-08 15:45:04 -0400

    Updated tests

--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -8,33 +8,34 @@ RSpec.describe Pawn, type: :model do
     end
 
     it 'should return true when moving two spaces forward on first move' do
-      pawn = FactoryGirl.create(:pawn, x_position: 4, y_position: 2)
+      pawn = FactoryGirl.create(:pawn, x_position: 4, y_position: 2, has_moved: false)
       expect(pawn.valid_move?(4, 4)).to eq true
     end
 
     it 'should return false if moving three spaces forward' do
       pawn = FactoryGirl.create(:pawn, x_position: 4, y_position: 2)
-      expect(pawn.valid_move?(4, 5)).to eq true
+      expect(pawn.valid_move?(4, 5)).to eq false
     end
 
     it 'should return false if moving two spaces forward after first move' do
-      pawn = FactoryGirl.create(:pawn, x_position: 4, y_position: 3)
-      expect(pawn.valid_move?(4, 5)).to eq true
+      pawn = FactoryGirl.create(:pawn, x_position: 4, y_position: 3, has_moved: true)
+      expect(pawn.valid_move?(4, 5)).to eq false
     end
 
     it 'should return false if moving one space backward' do
       pawn = FactoryGirl.create(:pawn, x_position: 4, y_position: 4)
-      expect(pawn.valid_move?(4, 3)).to eq true
+      expect(pawn.valid_move?(4, 3)).to eq false
     end
 
     it 'should return true if moving one space diagonally forward when capturing' do
-      pawn = FactoryGirl.create(:pawn, x_position: 4, y_position: 4)
-      expect(pawn.valid_move?(5, 5)).to eq true
+      white_pawn = FactoryGirl.create(:pawn, x_position: 4, y_position: 4, is_white: true)
+      black_pawn = FactoryGirl.create(:pawn, x_position: 5, y_position: 5, is_white: false)
+      expect(white_pawn.valid_move?(5, 5)).to eq true
     end
 
     it 'should return false if moving one space diagonally forward when not capturing' do
       pawn = FactoryGirl.create(:pawn, x_position: 4, y_position: 4)
-      expect(pawn.valid_move?(5, 5)).to eq true
+      expect(pawn.valid_move?(5, 5)).to eq false
     end
   end
 end
