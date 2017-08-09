a36341b68ae85646c253b5828e20e7c42141d3fa
Robert Polacheck <rpolacheck@gmail.com>
2017-08-09 21:08:26 +0000

    Functioning Rook piece

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,10 +1,17 @@
 class Rook < Piece 
 
-  def valid_move?(piece)
-    if @piece.type == "ROOK"
-      if @piece.horizontal_or_vertical?
-        piece.horizontal_or_vertical_obstruction?
-      end
-    end
+  def valid_move?(x_destination, y_destination)
+    (vertical(x_destination, y_destination) || horizontal(x_destination, y_destination)) && !obstructed?(x_destination, y_destination)
   end
+
+  def vertical(x_destination, y_destination)
+    (x_position == x_destination) && (y_position != y_destination)
+  end
+
+  def horizontal(x_destination, y_destination)
+    (y_position == y_destination) && (x_position != x_destination)
+  end
+
+
 end
+
