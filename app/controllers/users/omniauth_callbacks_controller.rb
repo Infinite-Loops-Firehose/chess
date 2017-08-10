305d0fed594f1c736957d8e49a2c3bf1222cc104
Dennis Ouyang <den2net@yahoo.com>
2017-08-10 21:51:59 +0000

    pushing existing code for facebook-login to troubleshoot

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,16 @@
+require 'omniauth'
+
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token
+
   def facebook
     @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.persisted? 
-      sign_in_and_redirect root_path
-      flash[:notice] = 'You have successfully logged in.'
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
-      flash[:error] = 'Login Failed.'
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
     end
   end
 end
