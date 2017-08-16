7dc90a974522752b6f3f222c99b30daa66bc4b3c
Dennis Ouyang <den2net@yahoo.com>
2017-08-16 03:58:58 +0000

    pushing current code for Bob and his mentor

--- app/assets/javascripts/master.js
+++ app/assets/javascripts/master.js
@@ -1,17 +1,33 @@
 
 $(function(){
+
+  function pieceHtml(piece){
+    var pieceId
+  }
+
   var pieceMoved = $('span#piece');
   // pieceMoved.on('mouseDown', function(){
   //   $('table#chessboard').css('z-index', '1');
   //   pieceMoved.css('z-index', '100');
   // });
+
+  var pieceId;
+
+  pieceMoved.mousedown(function(e){
+    pieceId = $(e.target).data('id');
+    console.log(pieceId);
+  })
+
   pieceMoved.draggable({
+
   });
 
   $('td').droppable(
     { accept: pieceMoved },
     {drop: function(e){
-      
+      $.ajax({
+        // url: '/pieces/' + pieceMoved.id
+      }) }
     }
   )
 
