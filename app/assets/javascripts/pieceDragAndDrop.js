7031871415c47b63625e197858528281c74061d5
Dennis Ouyang <den2net@yahoo.com>
2017-08-17 04:18:17 +0000

    HTML is updating but piece_image is displayed in wrong place

---
+++ app/assets/javascripts/pieceDragAndDrop.js
@@ -0,0 +1,33 @@
+
+$(".games.show").ready(function(){
+
+  var pieces = $('span#piece');
+  var pieceHTML;
+  var pieceId;
+
+  pieces.mousedown(function(e){
+    pieceHTML = e.target;
+    pieceId = $(e.target).data('id');
+  })
+
+  pieces.draggable();
+
+  $('td').droppable(
+    { accept: pieces },
+    {drop: function(e){
+      destSqIdNum = parseInt(e.target.id);
+      destSqX = Math.trunc(destSqIdNum / 10);
+      destSqY = destSqIdNum % 10;
+      $.ajax({
+        url: '/pieces/' + pieceId,
+        method: "PUT",
+        data: { piece: { x_position: destSqX, y_position: destSqY } },
+        success: function(){
+          console.log( pieceHTML );
+          e.target.append( pieceHTML );
+        }
+      })
+    }}
+  )
+
+});
