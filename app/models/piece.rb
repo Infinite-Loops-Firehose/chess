26cf27bc584dbd5ca61a04b49f31fca1178f8cfc
MelsenC <melsen.carlsburg@gmail.com>
2017-08-18 11:01:30 +0000

    fixed grammar error in piece.rb error message

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -14,7 +14,7 @@ class Piece < ApplicationRecord
 
   def move_to!(new_x, new_y)
     unless valid_move?(new_x, new_y)
-      raise ArgumentError, "That is invalid move for #{type}"
+      raise ArgumentError, "That is an invalid move for #{type}"
     end
     unless square_occupied?(new_x, new_y)
       update_attributes(x_position: new_x, y_position: new_y)
