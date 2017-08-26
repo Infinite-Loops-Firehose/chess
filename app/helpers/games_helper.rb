bacbf3fe2db48aa2b6ae457f89253ea8496e3469
Robert Polacheck <rpolacheck@gmail.com>
2017-08-26 00:42:44 +0000

    Still messing around with gravatar

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,9 +1,10 @@
 module GamesHelper
-
-  def gravatar_for(user)
-      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-      image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  class Devise::Registration
+    def gravatar_for(user, options = { size: 50})
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        size = options[:size]
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
   end
-
 end
