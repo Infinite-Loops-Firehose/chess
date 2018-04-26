d2f1c02d6545c036195903b8af26a026513aae00
Dennis Ouyang <den2net@yahoo.com>
2018-04-26 03:08:11 +0000

    adding heroku sendgrid addon and config for production emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,13 +2,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD']
+    :address => "localhost",
+    :port => 1025,
+    :domain => "https://infinite-loops-chess.herokuapp.com"
   }
 
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,14 +5,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default charset: "utf-8"
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
+    domain: "heroku.com",
+    address: "smtp.sendgrid.net",
     port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 
 
